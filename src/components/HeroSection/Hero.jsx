9d1d615e1cc18f52b652560bf85db9c17db7dca7
Shahriar-Aqib <shahriaraqib25@gmail.com>
2025-08-06 21:47:25 +0600

    sidebar updated

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -1,7 +1,7 @@
 function HeroSec() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center mt-10">Student Panel</h1>
+      <h1 className="text-4xl font-bold text-center mt-10">Hero Section</h1>
     </div>
   );
 }
