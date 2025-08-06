5f9700c7ea5d7dfb7dcfb4f8825b68940a7a4c23
Shahriar-Aqib <shahriaraqib25@gmail.com>
2025-08-06 22:19:58 +0600

    modified sidebar and Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ function Card() {
     <div className='p-4' >
         <div className="bg-white bg-opacity-80 border border-gray-200 rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow duration-300">
       <p> Current GPA</p>
-      <p>3.36</p>
+      <p className='text-4xl'>3.36</p>
       <p>+0.12 From Previous Semester</p>
       </div>
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./Sidebar.module.css";
-import {NotepadText,} from "lucide-react"
+import {NotepadText,ListChecks,StickyNote,UserCheck} from "lucide-react"
 function Sidebar(){
 
     return(
         <div className="w-70 h-screen bg-gray-50">
             <ul>
                 <li className={styles.list}><NotepadText /><button>Student Dashboard</button></li>
-                <li className={styles.list}><button>My Plan</button></li>
-                <li className={styles.list}><button>Academic History</button></li>
-                <li className={styles.list}><button>Advisor Notes</button></li>
+                <li className={styles.list}><ListChecks /><button>My Plan</button></li>
+                <li className={styles.list}><StickyNote /><button>Academic History</button></li>
+                <li className={styles.list}><UserCheck /><button>Advisor Notes</button></li>
                 
             </ul>
         </div>
