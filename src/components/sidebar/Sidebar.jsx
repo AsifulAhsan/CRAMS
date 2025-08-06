494e58a19bb4c8c9d2fe96f3a02389e7a1d2447d
Shahriar-Aqib <shahriaraqib25@gmail.com>
2025-08-06 19:56:55 +0600

    added sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import styles from "../sidebar/Sidebar.module.css";
 function Sidebar(){
 
     return(
         <div>
-            <p>Hello</p>
+            <div className="w-50 h-screen bg-[#ffff] shadow-md">
+                <ul>
+                    <li className="subject"><button>Student Dashboard</button></li>
+                    <li><button>My Plan</button></li>
+                    <li><button>Academic History</button></li>
+                    <li><button>Advisor Notes</button></li>
+                </ul>
+                <p>Hello</p>
+            </div>
+            
         </div>
 
 
