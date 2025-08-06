9a957dc982e0dbf62b993dbbefdd33f919059843
Alchemist-Kira <imranabid212@gmail.com>
2025-08-06 21:22:44 +0600

    added Herosec

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import HeroSec from "./components/HeroSection/Hero";
-import Card from "./components/Card/Card"
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Sidebar/>
+      <Sidebar />
       <Card />
-     
+      <HeroSec />
     </>
   );
 }
