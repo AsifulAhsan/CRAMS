47257367f08df50eb6e5df76a5bd3ae058788ba7
Alchemist-Kira <imranabid212@gmail.com>
2025-08-06 17:22:36 +0600

    added Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
+import HeroSec from "./components/HeroSection/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
-     
+      <Sidebar />
+      <HeroSec />
     </>
-    
   );
 }
 export default App;
-
