1b643045043db7129e3ce3cb0ade117632945529
Shahriar-Aqib <shahriaraqib25@gmail.com>
2025-08-06 20:54:06 +0600

    editted sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import styles from "./Sidebar.module.css";
+import {NotepadText,} from "lucide-react"
 function Sidebar(){
 
     return(
-        <div>
+        <div className="w-70 h-screen bg-gray-50">
             <ul>
-                <li><button>Student Dashboard</button></li>
-                <li><button>My Plan</button></li>
-                <li><button>Academic History</button></li>
-                <li><button>Advisor Notes</button></li>
+                <li className={styles.list}><NotepadText /><button>Student Dashboard</button></li>
+                <li className={styles.list}><button>My Plan</button></li>
+                <li className={styles.list}><button>Academic History</button></li>
+                <li className={styles.list}><button>Advisor Notes</button></li>
                 
             </ul>
         </div>
