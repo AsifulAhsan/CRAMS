b287ffa5dcc4118e9d4e29828a51e060df8680dc
Shahriar-Aqib <shahriaraqib25@gmail.com>
2025-08-07 19:36:51 +0600

    added Advisor Panel

---
+++ src/components/Advisor/AdvisorPanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdvisorNav from "./AdvisorNav"
+
+const AdvisorPanel = () => {
+    return (
+        <div>
+            <AdvisorNav/>
+        </div>
+    );
+};
+
+export default AdvisorPanel;
