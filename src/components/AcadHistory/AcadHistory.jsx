84dd3f89b7fb807496aa8fc32b402eca17392a04
Asiful Ahsan <asifulahsan@outlook.com>
2025-08-07 23:33:31 +0600

    Changes made to AcadHistory Table

--- src/components/AcadHistory/AcadHistory.jsx
+++ src/components/AcadHistory/AcadHistory.jsx
@@ -41,37 +41,54 @@ function AcadHistory() {
         />
       </div>
       <table className="flex justify-between mx-4 bg-white p-4 rounded-xl border-1 border-gray-200">
-        <div className="flex-col mx-4 my-2 w-full"> {/*First Col*/}
-            <p className="text-xl mb-8 ">Course Code</p>
-            <p className="mt-4 mb-8">CS101</p>
-            <p className=" mb-8">EC101</p>
-            <p className=" mb-8">MA101</p>
-            <p className=" mb-8">EN101</p>
-            <p className=" mb-8">HI101</p>
+        
+        <div className="flex-col mx-4 my-2 w-full">
+          {/*First Col*/}
+          <p className="text-xl mb-8 ">Course</p>
+          <p className="mt-4 mb-8">
+            CS101 <br />
+            Programming 1
+          </p>
+          <p className=" mb-8">
+            EC101 <br />
+            Microeconomics
+          </p>
+          <p className=" mb-8">
+            MA101 <br />
+            Calculus 1
+          </p>
+          <p className=" mb-8">
+            EN101 <br />
+            English Literature 1
+          </p>
+          <p className=" mb-8">
+            HI101 <br />
+            History Teaching 1
+          </p>
         </div>
         <div className="flex-col mx-4 my-2 w-full">
-            <p className="text-xl mb-8 ">Course title</p>
-            <p className="mt-4 mb-8">Programming 1</p>
-            <p className=" mb-8">Microeconomics</p>
-            <p className=" mb-8">Calculus 1</p>
-            <p className=" mb-8">Composition 1</p>
-            <p className=" mb-8">History Teaching 1</p>
+          <p className="text-xl mb-8 ">Instructor</p>
+          <p className="mt-4 mb-16">Md. Ali Khan</p>
+          <p className=" mb-16">Rina Begum</p>
+          <p className=" mb-16">Mohammad Rashedul Islam</p>
+          <p className=" mb-16">Jamal Assad</p>
+          <p className=" mb-16">Shariar Mohammad Aqib</p>
         </div>
         <div className="flex-col mx-4 my-2 w-full">
-            <p className="text-xl mb-8 ">Instructor</p>
-            <p className="mt-4 mb-8">Md. Ali Khan</p>
-            <p className=" mb-8">Rina Begum</p>
-            <p className=" mb-8">Mohammad Rashedul Islam</p>
-            <p className=" mb-8">Jamal Assad</p>
-            <p className=" mb-8">Shariar Mohammad Aqib</p>
+          <p className="text-xl mb-8 ">Credits</p>
+          <p className="mt-4 mb-16">3</p>
+          <p className=" mb-16">3</p>
+          <p className=" mb-16">4</p>
+          <p className=" mb-16">3</p>
+          <p className=" mb-16">2</p>
         </div>
         <div className="flex-col mx-4 my-2 w-full">
-            <p className="text-xl mb-8 ">Credits</p>
-            <p className="mt-4 mb-8">3</p>
-            <p className=" mb-8">3</p>
-            <p className=" mb-8">4</p>
-            <p className=" mb-8">3</p>
-            <p className=" mb-8">2</p>
+          <p className="text-xl mb-8 ">Grade</p>
+          <p className="mt-4 mb-16">A</p>
+          <p className=" mb-16">A-</p>
+          <p className=" mb-16">B</p>
+          <p className=" mb-16">A+</p>
+          <p className=" mb-16">A+</p>
         </div>
       </table>
     </>
