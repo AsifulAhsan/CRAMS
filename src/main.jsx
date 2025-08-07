99d2a968c56715d88e84824b5b3b88fee2465e15
Alchemist-Kira <imranabid212@gmail.com>
2025-08-07 16:52:26 +0600

    added admin panel

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CourseManage from "./components/CourseManage/CourseManage.jsx";
 import StudentDashboard from "./components/StudentDashboard/StudentDashboard.jsx";
+import AdminPage from "./components/AdminPage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <StudentDashboard /> },
       { path: "/course-manage", element: <CourseManage /> },
+      { path: "/admin", element: <AdminPage /> },
     ],
   },
 ]);
