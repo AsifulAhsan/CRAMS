daf2118f6fa9e75614c455206c7fb43928ebfffe
Shahriar-Aqib <shahriaraqib25@gmail.com>
2025-08-09 02:49:43 +0600

    added model etc to backend

--- Backend/Controller/Course.controller.js
+++ Backend/Controller/Course.controller.js
@@ -1,8 +1,9 @@
-import Course from "./Model/Course.model";
+//import Course from "./Model/Course.model.js";
+import Course from "../Model/Course.model.js";
 export const getCourse=async(req,res)=>{
     try {
-        const Course=await Course.find()
-        res.status(200).json(Course)
+        const Course2=await Course.find()
+        res.status(200).json(Course2)
     } catch (error) {
         console.log("error: ",error)
         res.status(500).json(error)
--- Backend/Route/Course.route.js
+++ Backend/Route/Course.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getCourse } from "../Controller/Course.controller"
+import { getCourse } from "../Controller/Course.controller.js"
 const router=express.Router();
 router.get("/", getCourse);
 
