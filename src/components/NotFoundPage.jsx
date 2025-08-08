f98b7c046d88c68161d91545c19e8b84fe005dda
Alchemist-Kira <imranabid212@gmail.com>
2025-08-08 15:33:21 +0600

    NOTFOUND PAGE

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -2,13 +2,35 @@ import { Link } from "react-router-dom";
 
 function NotFoundPage() {
   return (
-    <div className="flex bg-[#1A1A1A] w-full m-0 justify-center items-center">
-      <div className="flex flex-col gap-8 items-center pl-6 mx-85 my-60">
-        <h1 className="text-white font-semibold text-4xl">
-          This Page Does Not Exist!
-        </h1>
-        <Link to={"/"}>
-          <button className="border-2 bg-white p-3 rounded-xl text-xl text-black font-bold cursor-pointer hover:text-white border-transparent hover:bg-[#035f64]">
+    <div className="w-full h-full">
+      <div className="flex flex-col w-full h-full justify-center items-center gap-4 p-8 rounded-lg bg-[#282C34]">
+        <div className="text-left">
+          <p className="font-mono text-lg text-[#56B6C2] w-full">
+            <span className="text-[#E06C75]">const</span>{" "}
+            <span className="text-[#C678DD]">error</span> ={" "}
+            <span className="text-[#D19A66]">404</span>;
+          </p>
+
+          <p className="font-mono text-lg text-[#56B6C2] w-full">
+            <span className="text-[#E06C75]">if</span> (
+            <span className="text-[#C678DD]">error</span>) {"{"}
+          </p>
+
+          <p className="font-mono text-lg text-[#5E6773] w-full pl-6">
+            // Page not found
+          </p>
+
+          <p className="font-mono text-lg text-[#ABB2BF] w-full pl-6">
+            <span className="text-[#E06C75]">throw new</span>{" "}
+            <span className="text-[#C678DD]">Error</span>(
+            <span className="text-[#98C379]">"Not Found"</span>);
+          </p>
+
+          <p className="font-mono text-lg text-[#ABB2BF] w-full">{"}"}</p>
+        </div>
+
+        <Link to={"/"} className="mt-4">
+          <button className="border-2 bg-[#ABB2BF] p-3 rounded-xl text-xl text-[#282C34] font-bold cursor-pointer hover:text-white border-transparent hover:bg-[#035f64] transition-colors duration-300">
             RETURN HOME
           </button>
         </Link>
