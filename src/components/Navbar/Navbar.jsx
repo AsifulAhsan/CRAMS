e6b9461b5a647452fd0018c2fd72c16d261dd409
Asiful Ahsan <asifulahsan@outlook.com>
2025-08-09 00:54:57 +0600

    CourseManage updated!

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar() {
           </div>
           <button
             onClick={handleLogout}
-            className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition"
+            className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition cursor-pointer"
           >
             Logout
           </button>
