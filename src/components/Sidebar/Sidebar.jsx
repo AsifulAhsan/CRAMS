eaa37cd7fe157d45f6fa8feb723866973233bf1d
Shahriar-Aqib <shahriaraqib25@gmail.com>
2025-08-09 10:55:44 +0600

    added

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "./Sidebar.module.css";
+import { NotepadText, ListChecks, StickyNote, UserCheck } from "lucide-react";
+import { NavLink, useLocation } from "react-router-dom";
+
+function Sidebar() {
+  const location = useLocation();
+
+  return (
+    <div className="fixed mt-18 w-70 h-screen bg-white border-r-1 border-gray-200">
+      <ul>
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            isActive && location.pathname === "/dashboard"
+              ? `${styles.list} ${styles.active}`
+              : styles.list
+          }
+        >
+          <li>
+            <NotepadText />
+            Student Dashboard
+          </li>
+        </NavLink>
+        <NavLink
+          to="/dashboard/courses"
+          className={({ isActive }) =>
+            isActive ? `${styles.list} ${styles.active}` : styles.list
+          }
+        >
+          <li>
+            <ListChecks />
+            Course Management
+          </li>
+        </NavLink>
+        <NavLink
+          to="/dashboard/academic-history"
+          className={({ isActive }) =>
+            isActive ? `${styles.list} ${styles.active}` : styles.list
+          }
+        >
+          <li>
+            <StickyNote />
+            Academic History
+          </li>
+        </NavLink>
+        <NavLink
+          to="/dashboard/advisor-notes"
+          className={({ isActive }) =>
+            isActive ? `${styles.list} ${styles.active}` : styles.list
+          }
+        >
+          <li>
+            <UserCheck />
+            Advisor Notes
+          </li>
+        </NavLink>
+      </ul>
+    </div>
+  );
+}
+export default Sidebar;
